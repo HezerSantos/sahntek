37f3c023317c22141bb9adaa32284dde8ba8be04
HezerSantos <HezerSantos24@gmail.com>
2025-05-23 20:37:30 -0500

    sh

--- frontend/src/components/universal/Footer.jsx
+++ frontend/src/components/universal/Footer.jsx
@@ -1,8 +1,66 @@
+import { Link } from "react-router-dom"
 const Footer = () => {
     return(
         <>
             <footer>
-                
+                <section>
+                    <div className="cr__original">
+                        <h3>SAHNTEK</h3>
+                        <ul>
+                            <li>
+                                <p>Powering Your Next Big Move</p>
+                            </li>
+                            <li>
+                                <p className="copy__right">© 2025 Sahntek. All rights reserved.</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer__border">
+                        <h3>COMPANY</h3>
+                        <ul>
+                            <li>
+                                <Link>People</Link>
+                            </li>
+                            <li>
+                                <Link>Blog</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer__border">
+                        <h3>QUICK LINKS</h3>
+                        <ul>
+                            <li>
+                                <Link>Home</Link>
+                            </li>
+                            <li>
+                                <Link>Shop Computers</Link>
+                            </li>
+                            <li>
+                                <Link>Pricing</Link>
+                            </li>
+                            <li>
+                                <Link>FAQ</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="support__links footer__border">
+                        <h3>SUPPORT LINKS</h3>
+                        <ul>
+                            <li>
+                                <Link>Shipping and Returns</Link>
+                            </li>
+                            <li>
+                                <Link>Privacy Policy</Link>
+                            </li>
+                            <li>
+                                <Link>Terms of Service</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="copyright__contianer">
+                        <p className="copy__right">© 2025 Sahntek. All rights reserved.</p>
+                    </div>
+                </section>
             </footer>
         </>
     )
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -3,6 +3,7 @@ import HomeBody from '../../components/home/HomeBody'
 import HomeComputerPlans from '../../components/home/HomeComputerPlans'
 import HomeHero from '../../components/home/HomeHero'
 import HomeSectionAdvance from '../../components/home/HomeSectionAdvance'
+import Footer from '../../components/universal/Footer'
 
 import NavBar from '../../components/universal/NavBar'
 const HomePage = () => {
@@ -15,6 +16,7 @@ const HomePage = () => {
                 <HomeSectionAdvance />
                 <HomeComputerPlans />
             </main>
+            <Footer/>
         </>
     )
 }
