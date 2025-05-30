4962785ae177c291348862e445336072f4cd9aec
HezerSantos <HezerSantos24@gmail.com>
2025-05-29 21:54:11 -0500

    Finished the promotional banner and made it responsive

--- frontend/src/components/browseComputers/promotionalBanner.jsx
+++ frontend/src/components/browseComputers/promotionalBanner.jsx
@@ -4,7 +4,12 @@ const PromotionalBanner = () => {
         <>
             <section className="browse__computer__promotional__banner">
                 <div>
-                    
+                    <h1>Explore Our Full Section</h1>
+
+                    <p>
+                        Continue browsing to see our full range of builds and <br />components.
+                        Find what suits your needs and <span>level up</span> your setup.
+                    </p>
                 </div>
             </section>
         </>
