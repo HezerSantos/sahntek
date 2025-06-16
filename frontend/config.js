9027c2fec9d0dda45e8fb043c4829bea5314e2e2
HezerSantos <HezerSantos24@gmail.com>
2025-06-16 03:15:42 -0500

    production v4.0 CORS ISSUE

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@ const config = {
       apiUrl: ""
     },
     development: {
-      apiUrl: "http://localhost:8080"
+      apiUrl: "https://sahntek-backend-production.up.railway.app"
     }
   };
 
