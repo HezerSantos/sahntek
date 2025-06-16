bdb9057db51cc9236cc41d9a95fe6c61ff42f186
HezerSantos <HezerSantos24@gmail.com>
2025-06-16 03:21:41 -0500

    production v4.1 CORS ISSUE

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -3,33 +3,27 @@ require('dotenv').config()
 const allowedOrigins = [
   "http://localhost:5173", 
   "https://sahntek.hallowedvisions.com",
+  "sahntek-frontend-production.up.railway.app"
 ];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin) {
-//       return callback(null, true);
-//     } 
-
-//     if(allowedOrigins.indexOf(origin) !== -1){
-//       return callback(null, true)
-//     }else {
-//       return callback(new Error("CORS not allowed for this origin"), false);
-//     }
-  
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie", "csrfToken"],
-//   credentials: true, // Allow cookies to be sent
-//   optionsSuccessStatus: 200
-// };
-
 const corsOptions = {
-  origin: 'https://sahntek.hallowedvisions.com', // This allows all origins
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    } 
+
+    if(allowedOrigins.indexOf(origin) !== -1){
+      return callback(null, true)
+    }else {
+      return callback(new Error("CORS not allowed for this origin"), false);
+    }
+  
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie", "csrfToken"],
   credentials: true, // Allow cookies to be sent
   optionsSuccessStatus: 200
 };
+
 
 module.exports = cors(corsOptions);
