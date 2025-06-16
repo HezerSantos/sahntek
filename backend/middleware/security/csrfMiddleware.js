3e5c175303d4e7a2609c24874f3066bc76f9ff9c
HezerSantos <HezerSantos24@gmail.com>
2025-06-16 03:49:48 -0500

    production v4.6 COOKIE ISSUE

--- backend/middleware/security/csrfMiddleware.js
+++ backend/middleware/security/csrfMiddleware.js
@@ -26,7 +26,7 @@ const csrf = async(req, res, next) => {
         maxAge: 5 * 1000 * 60, 
         sameSite: "None",
         path: "/",
-        domain: ".hallowedvisions.com"
+        domain: "sahntek.hallowedvisions.com"
     })
 
     res.cookie("__Host.refresh-csrf-token", __HostRefreshCsrfToken, {
@@ -35,7 +35,7 @@ const csrf = async(req, res, next) => {
         maxAge: 5 * 1000 * 60, 
         sameSite: "None",
         path: "/",
-        domain: ".hallowedvisions.com"
+        domain: "sahntek.hallowedvisions.com"
     })
 
     res.status(200).send()
