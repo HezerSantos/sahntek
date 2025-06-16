a467ff55be3aab5772d315cb18308784119a4ca6
HezerSantos <HezerSantos24@gmail.com>
2025-06-16 02:00:25 -0500

    production v3.3 -- CORS ISSUE

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://127.0.0.1:5173", 
-  "https://sahntek.hallowedvisions.com/"
+  "https://sahntek.hallowedvisions.com"
 ];
 
 const corsOptions = {
@@ -25,4 +25,13 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-module.exports = cors(corsOptions);
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", 'https://sahntek.hallowedvisions.com');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie, Set-Cookie, csrfToken");
+    return res.status(200).end();
+  }
+  cors(corsOptions)(req, res, next);
+};
