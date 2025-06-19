deb621ded1f9dc38c404c0e890a9f16c7440dbab
HezerSantos <HezerSantos24@gmail.com>
2025-06-18 21:47:56 -0500

    cookie issue fix

--- backend/controllers/computer/GET/getAllComputers.js
+++ backend/controllers/computer/GET/getAllComputers.js
@@ -113,8 +113,7 @@ exports.getAllComputers = async(req, res, next) => {
 
         // const test = await Promise.all([...proPromises, ...premiumPromises, ...advancedPromises])
         // console.timeEnd("getAllComputers");
-
-        console.log(signedPro)
+        
         res.json({
             pro: signedPro,
             advanced: signedAdvanced,
